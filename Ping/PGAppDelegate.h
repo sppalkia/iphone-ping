b92db119df6668556bfef71b8023b647d34bf26d
Shoumik Palkar <shoumikpalkar@gmail.com>
2014-02-12 20:44:18 -0800

    Initial Commit

---
+++ Ping/PGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PGAppDelegate.h
+//  Ping
+//
+//  Created by Shoumik Palkar on 2/12/14.
+//  Copyright (c) 2014 Shoumik Palkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ping/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ping
+//
+//  Created by Shoumik Palkar on 2/12/14.
+//  Copyright (c) 2014 Shoumik Palkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PGAppDelegate class]));
+    }
+}
